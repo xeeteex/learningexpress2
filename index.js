f6cf815b6784597c8f37acdb774473629e5e2b07
Kshitij <xeeteexstha@gmail.com>
2025-02-13 23:26:54 +0545

    day3 express basics done

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ middleware
    and also has access to next valid middleware
    */
 
-const checkAuthentication = (req, res,next) => {
+const checkAuthentication = (req, res, next) => {
   console.log("check authentication..");
 
-  let loggedIn = false;
+  let loggedIn = true;
   if (!loggedIn) {
     // else lekhnu pardena this style
     return res.status(401).send({
@@ -19,33 +19,40 @@ const checkAuthentication = (req, res,next) => {
     });
   }
 
-  next()
+  next();
 };
 
+const checkBuyer = (req, res, next) => {
+  let role = "buyer";
+  console.log("check buyer");
+
+  if (role === "buyer") {
+    next();
+  } else {
+    res.status(403).send({
+      msg: "forbidden",
+    });
+  }
+};
+
+app.use(express.json() )
+// app.use(checkBuyer);
 //global middleware
 //app.use(checkAuthentication); //middleware call garne tarika
 
 /* protected routes */
 
-// const checkAuthentication= (res) => {
-//   let loggedIn = false
-//   if(!loggedIn){  // else lekhnu pardena this style
-//    return res.status(401).send({
-//       msg: "unauthenticated"
-//     })
-//   }
-// }
- 
-/**checkAuthentication if only used in certain functions
- * otherwise use app.use
- */
-
-app.get("/api/dashboard",checkAuthentication,  (req, res) => {
+app.get("/api/dashboard", checkAuthentication, (req, res) => {
   res.send("dashboard data");
 });
 
-app.get("/api/orders",checkAuthentication, (req, res) => {
-  res.send("dashboard data");
+app.get("/api/orders", checkAuthentication, checkBuyer, (req, res) => {
+  res.send("orders data");
+});
+
+app.post("/api/orders", checkAuthentication, checkBuyer, (req, res) => {
+  console.log("req.body", req.body);
+  res.send("create orders  ");
 });
 
 app.get("/api/test", (req, res) => {
@@ -55,3 +62,16 @@ app.get("/api/test", (req, res) => {
 app.listen(8000, () => {
   console.log("server started");
 });
+
+// const checkAuthentication= (res) => {
+//   let loggedIn = false
+//   if(!loggedIn){  // else lekhnu pardena this style
+//    return res.status(401).send({
+//       msg: "unauthenticated"
+//     })
+//   }
+// }
+
+/**checkAuthentication if only used in certain functions
+ * otherwise use app.use
+ */
